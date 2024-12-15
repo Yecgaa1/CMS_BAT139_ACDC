eb038e8af94693b2f2d7d3d97ab42805de80c518
xtx <2656823393@qq.com>
2024-12-15 16:32:55 +0800

    先改出串口发送

--- code/head/3_user_h/change.h
+++ code/head/3_user_h/change.h
@@ -29,7 +29,6 @@ void DebugUse(void);
 #define I6_OUT 193 * COM_REAL_VACIN_RMS_SCAL 
 
 int32_t Get_PLL_Cos(PLL_Ctrl_Var_t *PLL_Info);
-extern uint16_t PFC_StartCount;
 extern int32_t save[512], save2[512];
 extern volatile uint32_t save_cnt, isONPWM, send_cnt;
 
